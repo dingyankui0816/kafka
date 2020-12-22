b89d621e03da518ab5406775bc3427b277feedee
Levi.Ding <dingyankui@troila.com>
2020-12-22 09:04:57 +0800

    add send msg controller

--- src/main/java/com/cn/kafka/controller/KafkaController.java
+++ src/main/java/com/cn/kafka/controller/KafkaController.java
@@ -1,10 +1,23 @@
 package com.cn.kafka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * @DESC: 文件介绍
+ * @DESC: Kafka Controller
  * @Auther: Levi.Ding
  * @Date: 2020/12/18 11:00
  */
+@RestController
 public class KafkaController {
 
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
+    @RequestMapping()
+    public void sendKafka(){
+        kafkaTemplate.send("test3","Hello World ! 丁");
+    }
 }
